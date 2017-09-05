0ee7fe59a27d11779baf120a0adfbd1ed6f6516b
laachax <laachax@gmail.com>
2017-09-05 00:39:28 -0500

    Added the all mighty lenny face

--- nsa/bot.py
+++ nsa/bot.py
@@ -24,7 +24,7 @@ async def on_message(message: discord.Message):
             await client.send_message(message.channel, content="Pong!")
         if args[0] == "!pong":
             await client.send_message(message.channel, content="Ping!")
-
-
+        if args[0] == "!lenny":
+            await.client.send_message(message.channel, content="( ͡° ͜ʖ ͡°)")
 def main():
     client.run(discord_api_token)
